b714fb628a2a883d8fbd10c265f6e9dc171088dd
sikandar388 <bone.implantes@gmail.com>
2025-04-28 18:32:08 +0500

    Use State does not change its state directly

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,20 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  
   let [counter, setCounter] = useState(8);
 
-  const addValue = ()=>{
-      if(counter<20){
-        setCounter(counter + 1);
-      }
-  }
-  function subtractValue(){
-    if(counter>0){
-      setCounter(counter-1);
+  const addValue = () => {
+    if (counter < 20) {
+      // setCounter(counter + 1);
+      // setCounter(counter + 1);
+      // setCounter(counter + 1);
+      // setCounter(counter + 1);
+      setCounter(preCounter => preCounter + 1);
+      setCounter(preCounter => preCounter + 1);
+      setCounter(preCounter => preCounter + 1);
+      setCounter(preCounter => preCounter + 1);
+    }
+  };
+  function subtractValue() {
+    if (counter > 0) {
+      setCounter(counter - 1);
     }
   }
 
@@ -23,11 +29,11 @@ function App() {
       <h1>Counter App</h1>
       <h2>Current Value : {counter} </h2>
       <button onClick={addValue}>Add Number... {counter}</button>
-      <br/>
+      <br />
       <button onClick={subtractValue}>Subtract Number... {counter}</button>
       <p>Footer : {counter}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
